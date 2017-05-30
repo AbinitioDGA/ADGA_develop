554540ec1377386a845383ec844b99dd5d434336
Josef Kaufmann <josef.kaufmann@tuwien.ac.at>
2017-05-30 11:03:23 +0200

    V_r extrpolation script also does Fourier transform now.

--- V_r_extrapol.py
+++ V_r_extrapol.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 '''
-This Skript reads a text file that contains r-vectors and U-values in the following format:
+This script reads one HDF5 file that contains nonlocal interaction U(r) and a list of r vectors.
 
-x   y   z   U_1   U_2   ...   U_ncomp
+It writes one file with extrapolated U(r) and one file with Fouriertransformed V(q).
+The constant U(0) is currently not subtracted. 
 
-ncomp has to be specified as a command line option --ncomp.
-
-Currently, also the number nd of orbitals has to be given as command line option (--nd),
-but this is likely to be dropped in the next version. It is necessary only to print it 
-in the output file, which is then read by Abinitio_DGA program.
-
-Further it is necessary to provide input and output filenames,
-and the number of r points for the extrapolated U(r) in x-, y- and z-direction.
+It is necessary to provide one input and two output filenames,
+and the number of r points for the extrapolated U(r) in x-, y- and z-direction,
+and the number of q points for the Fourier transform in qx-, qy- and qz-direction.
 
 The type of the fit function cannot be specified interactively, but only in the script,
 approx. line 100:
@@ -19,12 +15,12 @@ approx. line 100:
 
 
 sample execution line:
-./V_r_extrapol.py --ncomp=6 --nd=3 ~/Data/SVO/U-00RR-vec U_r_extrapol.dat 10 10 10
+./V_r_extrapol.py  ~/Data/SVO/U-00RR-vec U_r_extrapol.dat V_r.hdf5 10 10 10 10 10 10
 
 
 
 Author: Josef Kaufmann,
-last change: Dec 9, 2016, Ekb
+last change: May 2017
 '''
 
 import numpy as np
@@ -44,48 +40,68 @@ __version__ = "1.0"
 parser = optparse.OptionParser(usage = "%prog [OPTIONS] input output nx ny nz",
                                version = ".".join(map(str,__version__)))
 
-parser.add_option("--ncomp", dest="ncomp",default=0,
-                 help="number of U-matrix components contained in the input file. (i.e. number of columns minus 3)")
+#parser.add_option("--ncomp", dest="ncomp",default=0,
+#                 help="number of U-matrix components contained in the input file. (i.e. number of columns minus 3)")
 
 parser.add_option("--nd", dest="nd",default=0, 
                  help="number of orbitals")
 
 
 options, args = parser.parse_args()
-if len(args) != 5:
-  parser.error("expected input and output filename and number of r points in each direction ")
+if len(args) != 9:
+  parser.error("expected input and output filename and number of r and q points in each direction ")
 
 filename_in=args[0]
 filename_out=args[1]
-nx=int(args[2])
-ny=int(args[3])
-nz=int(args[4])
+outfilename_vq=args[2]
+nx=int(args[3])
+ny=int(args[4])
+nz=int(args[5])
+nqx=int(args[6])
+nqy=int(args[7])
+nqz=int(args[8])
 nr=(2*nx+1)*(2*ny+1)*(2*nz+1)
-ncomp=int(options.ncomp) # number of components of U(r)
+#ncomp=int(options.ncomp) # number of components of U(r)
 nd=int(options.nd) # number of orbitals just needed to write output file.
 
-print nd,'Bands'
-print 'U matrix has',ncomp,'components'
+#print nd,'Bands'
+#print 'U matrix has',ncomp,'components'
 
 
-# read the input data
-infile=open(filename_in,'r')
-data=infile.readlines()
-n = len(data)
-r_vec=np.zeros((n,3),dtype=np.float_)
-r_abs=np.zeros((n),dtype=np.float_)
-U_r_in=np.zeros((n,ncomp),dtype=np.float_)
-for i in xrange(n):
-  r_vec[i]=data[i].split()[:3]
-  r_abs[i]=np.linalg.norm(r_vec[i])
-  U_r_in[i]=np.float_(data[i].split()[-ncomp:])
-infile.close()
+# read the input data from text file
+#infile=open(filename_in,'r')
+#data=infile.readlines()
+#n = len(data)
+#r_vec=np.zeros((n,3),dtype=np.float_)
+#r_abs=np.zeros((n),dtype=np.float_)
+#U_r_in=np.zeros((n,ncomp),dtype=np.float_)
+#for i in xrange(n):
+#  r_vec[i]=data[i].split()[:3]
+#  r_abs[i]=np.linalg.norm(r_vec[i])
+#  U_r_in[i]=np.float_(data[i].split()[-ncomp:])
+#infile.close()
+
+# read the input data from HDF5 file
+import h5py
+infile=h5py.File(filename_in,'r')
+groups=[key for key in infile.keys() if key.isdigit()]
+ncomp=len(groups)
+print 'Nonlocal interaction has',ncomp,'components'
+r_vec=infile['.axes/R-points'].value
+r_abs=np.array(map(np.linalg.norm,r_vec))
+print r_abs.shape
+n=r_vec.shape[0]
+U_r_in=np.zeros((n,ncomp),dtype=np.float)
+for igr,gr in enumerate(groups):
+  U_r_in[:,igr]=infile[gr+'/value'].value
+
 
 # plot the input data vs abs(r)
 plotcomp=0 # component to plot
 f1=plt.figure(1)
 plt.plot(r_abs,U_r_in[:,plotcomp].real,marker='.',linestyle='None')
 f1.show()
+
 
 # do the fit
 # since the function we use cannot do a constrained optimization,
@@ -113,10 +129,13 @@ if fitfun==yukawa:
   rdata=np.zeros((n-1),dtype=np.float_)
   j=0
   for i in xrange(n):
-    if r_abs[i] >= 0.001:
+    #if r_abs[i] >= 0.001:
+    if r_abs[i] >= 3:
       ydata[j]=U_r_in[i]
       rdata[j]=r_abs[i]
       j=j+1
+  ydata=ydata[:j-1]
+  rdata=rdata[:j-1]
 else:
   ydata=np.zeros((n,ncomp),dtype=np.float_)
   rdata=np.zeros((n),dtype=np.float_)
@@ -138,8 +157,14 @@ f2=plt.figure(2)
 plt.plot(np.linspace(min(rdata),max(rdata)),fitfun(np.linspace(min(rdata),max(rdata)),fitparam[plotcomp,0],fitparam[plotcomp,1]))
 plt.plot(r_abs,U_r_in[:,plotcomp].real,marker='.',linestyle='None')
 f2.show()
-
-
+print fitparam[plotcomp],min(rdata),max(rdata),fitfun(max(rdata),fitparam[plotcomp,0],fitparam[plotcomp,1])
+#for iplt in xrange(ncomp):
+#  plt.figure(iplt+10)
+#  plt.plot(np.linspace(min(rdata),max(rdata)),fitfun(np.linspace(min(rdata),max(rdata)),fitparam[iplt,0],fitparam[iplt,1]))
+#  plt.plot(r_abs,U_r_in[:,iplt].real,marker='.',linestyle='None')
+#  plt.show()
+#raw_input()
+#sys.exit()
 # Now we evaluate the fit function on a grid specified by the command line arguments
 
 # First we need the lattice spacing
@@ -159,6 +184,7 @@ U_ext=np.zeros((2*nx+1,2*ny+1,2*nz+1,ncomp),dtype=np.float_)
 outfile=open(filename_out,'w')
 outfile.write('{} {} {} {} {} \n'.format(nr,nd,a,b,c))
 for i in xrange(2*nx+1):
+  print i
   for j in xrange(2*ny+1):
     for k in xrange(2*nz+1):
       r_ext[i,j,k,:]=[(i-nx)*a,(j-ny)*b,(k-nz)*c]
@@ -173,7 +199,6 @@ for i in xrange(2*nx+1):
       outfile.write(('{:08f}\t{:08f}\t{:08f}\n'+ncomp*'{:14f}\t'+'\n').format(r_ext[i,j,k,0],r_ext[i,j,k,1],r_ext[i,j,k,2],*U_ext[i,j,k]))
 outfile.close()
 
-
 f3=plt.figure(3)
 plt.plot(r_ext[:,ny,nz,0],U_ext[:,ny,nz,plotcomp],marker='.',linestyle='None',label='U(x)')
 # The plot in z dirction is done separately, because in HgBa2CuO4 we have different lattice spacing
@@ -183,5 +208,46 @@ plt.legend()
 plt.xlim(-10,10)
 plt.xlabel('x,z')
 f3.show()
+
+U_ext=U_ext.reshape((-1,ncomp))
+r_ext=r_ext.reshape((-1,3))
+r_ext_h5=h5py.File('r_ext.hdf5','w')
+for igr,gr in enumerate(groups):
+  r_ext_h5[gr]=U_ext[...,igr]
+r_ext_h5.create_group('.axes')
+r_ext_h5['.axes/R-points']=r_ext
+
+
+
+print 'Extrapolation done'
+print 'Now Fourier transform...'
+# TODO use a FFT routine here (this is currently not done in order to have more flexibility with the grids.)
+Vq=np.zeros((nqx,nqy,nqz,ncomp),dtype=np.complex)
+qvec=np.zeros((nqx,nqy,nqz,3),dtype=np.float)
+for i in xrange(nqx):
+  print i
+  for j in xrange(nqy):
+    for k in xrange(nqz):
+      qvec[i,j,k]=np.array([float(i)/(a*nqx),float(j)/(b*nqy),float(k)/(c*nqz)]) # the q grid has to be rescaled with lattice spacing
+      Vq[i,j,k,:]=np.dot(np.exp(2*1j*np.pi*np.dot(r_ext,np.array([qvec[i,j,k]]).transpose())).transpose(),U_ext)
+
+print 'writing output to',outfilename_vq
+outfile_vq=h5py.File(outfilename_vq,'w')
+outfile_vq.create_group('.axes')
+outfile_vq['.axes/Q-points']=qvec.reshape((-1,3))
+for igr,gr in enumerate(groups):
+  outfile_vq[gr]=Vq[...,igr].reshape(-1)
+outfile.close()
+
+f4=plt.figure(4)
+plt.plot(Vq[:,0,0,plotcomp].real,linestyle='None',marker='.')
+plt.plot(Vq[:,0,0,plotcomp].imag,linestyle='None',marker='.')
+f4.show()
+
+print Vq[:,0,0,0]
+
+print 'done, press Enter to exit'
+
+
 
 raw_input()
