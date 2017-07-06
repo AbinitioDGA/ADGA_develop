08ad84252ef6c81c859d790180d2a2a45f99d88f
Josef Kaufmann <e1026763@student.tuwien.ac.at>
2017-07-06 14:51:27 +0200

    Several modifications in V_r_extrapol.py; choose yukawa_2 for hbco, yukawa_3 for svo.

--- V_r_extrapol.py
+++ V_r_extrapol.py
@@ -26,7 +26,6 @@ last change: May 2017
 import numpy as np
 from scipy.optimize import curve_fit
 
-from matplotlib import pyplot as plt
 
 
 import sys
@@ -43,13 +42,18 @@ parser = optparse.OptionParser(usage = "%prog [OPTIONS] input output nx ny nz",
 #parser.add_option("--ncomp", dest="ncomp",default=0,
 #                 help="number of U-matrix components contained in the input file. (i.e. number of columns minus 3)")
 
-parser.add_option("--nd", dest="nd",default=0, 
-                 help="number of orbitals")
+parser.add_option("--nd", dest="nd",default=0, help="number of orbitals")
+parser.add_option("--interactive",dest="show_plots",default=False,action="store_true", help="switch on interactive plots")
 
 
 options, args = parser.parse_args()
 if len(args) != 9:
   parser.error("expected input and output filename and number of r and q points in each direction ")
+
+
+if options.show_plots:
+  from matplotlib import pyplot as plt
+
 
 filename_in=args[0]
 filename_out=args[1]
@@ -98,20 +102,28 @@ for igr,gr in enumerate(groups):
 
 # plot the input data vs abs(r)
 plotcomp=0 # component to plot
-f1=plt.figure(1)
-plt.plot(r_abs,U_r_in[:,plotcomp].real,marker='.',linestyle='None')
-f1.show()
+if options.show_plots:
+  f1=plt.figure(1)
+  plt.plot(r_abs,U_r_in[:,plotcomp].real,marker='.',linestyle='None')
+  f1.show()
 
 
 # do the fit
 # since the function we use cannot do a constrained optimization,
 # I add a "penalization" if b<=0.
 # I very much hope that this is correct.
-def yukawa(r,a,b):
+def yukawa_2(r,a,b):
   penalization = 0.
   if b<=0.:
     penalization=1.e20
   return a*np.exp(-b*np.abs(r))/np.abs(r) + penalization
+
+def yukawa_3(r,a,b,c):
+  penalization = 0.
+  if b<=0.:
+    penalization=1.e20
+  return a*np.exp(-b*np.abs(r))/np.abs(r) + penalization + c
+
 
 def simple_exp(r,a,b):
   penalization = 0.
@@ -121,16 +133,16 @@ def simple_exp(r,a,b):
 #  return 1.95*np.exp(-b*np.abs(r))
 
 
-fitfun=yukawa
+fitfun=yukawa_2
 
-if fitfun==yukawa:
+if fitfun==yukawa_2 or fitfun==yukawa_3:
 # if we fit to yukawa, take out the value at r=0, because we cannot fit it otherwise.
   ydata=np.zeros((n-1,ncomp),dtype=np.float_)
   rdata=np.zeros((n-1),dtype=np.float_)
   j=0
   for i in xrange(n):
-    #if r_abs[i] >= 0.001:
-    if r_abs[i] >= 3:
+    if r_abs[i] >= 0.001:
+    #if r_abs[i] >=3.:
       ydata[j]=U_r_in[i]
       rdata[j]=r_abs[i]
       j=j+1
@@ -145,19 +157,25 @@ else:
     rdata[j]=r_abs[i]
     j=j+1
 
-fitparam=np.zeros((ncomp,2),dtype=float)
-fitcov = np.zeros((ncomp,2,2),dtype=float)
+n_fitparam=2
+if fitfun==yukawa_3:
+  n_fitparam=3
+fitparam=np.zeros((ncomp,n_fitparam),dtype=float)
+fitcov = np.zeros((ncomp,n_fitparam,n_fitparam),dtype=float)
 for iu in xrange(ncomp):
   fitparam[iu], fitcov[iu] = curve_fit(fitfun,rdata,ydata[:,iu])
   print 'component',iu
   print 'opt. fit params.',fitparam[iu]
   print 'covariance',fitcov[iu]
+  if n_fitparam==3:
+    fitparam[iu,2]=0.
 
-f2=plt.figure(2)
-plt.plot(np.linspace(min(rdata),max(rdata)),fitfun(np.linspace(min(rdata),max(rdata)),fitparam[plotcomp,0],fitparam[plotcomp,1]))
-plt.plot(r_abs,U_r_in[:,plotcomp].real,marker='.',linestyle='None')
-f2.show()
-print fitparam[plotcomp],min(rdata),max(rdata),fitfun(max(rdata),fitparam[plotcomp,0],fitparam[plotcomp,1])
+if options.show_plots:
+  f2=plt.figure(2)
+  plt.plot(np.linspace(min(rdata),max(rdata)),fitfun(np.linspace(min(rdata),max(rdata)),*fitparam[plotcomp]))
+  plt.plot(r_abs,U_r_in[:,plotcomp].real,marker='.',linestyle='None')
+  f2.show()
+print fitparam[plotcomp],min(rdata),max(rdata),fitfun(max(rdata),*fitparam[plotcomp])
 #for iplt in xrange(ncomp):
 #  plt.figure(iplt+10)
 #  plt.plot(np.linspace(min(rdata),max(rdata)),fitfun(np.linspace(min(rdata),max(rdata)),fitparam[iplt,0],fitparam[iplt,1]))
@@ -189,25 +207,26 @@ for i in xrange(2*nx+1):
     for k in xrange(2*nz+1):
       r_ext[i,j,k,:]=[(i-nx)*a,(j-ny)*b,(k-nz)*c]
       for iu in xrange(ncomp):
-        U_ext[i,j,k,iu]=fitfun(np.linalg.norm(r_ext[i,j,k]),fitparam[iu,0],fitparam[iu,1])
+        U_ext[i,j,k,iu]=fitfun(np.linalg.norm(r_ext[i,j,k]),*fitparam[iu])
       if np.linalg.norm(r_ext[i,j,k,:]) < np.max(r_abs):
         for l in xrange(n):
-          if np.allclose(r_vec[l],r_ext[i,j,k],atol=np.min([a,b,c])/10):
+          if np.allclose(r_vec[l],r_ext[i,j,k],atol=np.min([a,b,c])/10) and np.linalg.norm(r_vec[l])<0.001:
             U_ext[i,j,k]=U_r_in[l]
             break
 # 
       outfile.write(('{:08f}\t{:08f}\t{:08f}\n'+ncomp*'{:14f}\t'+'\n').format(r_ext[i,j,k,0],r_ext[i,j,k,1],r_ext[i,j,k,2],*U_ext[i,j,k]))
 outfile.close()
 
-f3=plt.figure(3)
-plt.plot(r_ext[:,ny,nz,0],U_ext[:,ny,nz,plotcomp],marker='.',linestyle='None',label='U(x)')
-# The plot in z dirction is done separately, because in HgBa2CuO4 we have different lattice spacing
-# and I want to show that the fit works also in z direction. (So it is actually unnecessary for SVO)
-#plt.plot(r_ext[nx,ny,:,2],U_ext[nx,ny,:,plotcomp],marker='*',linestyle='None',label='U(z)')
-plt.legend()
-plt.xlim(-10,10)
-plt.xlabel('x,z')
-f3.show()
+if options.show_plots:
+  f3=plt.figure(3)
+  plt.plot(r_ext[:,ny,nz,0],U_ext[:,ny,nz,plotcomp],marker='.',linestyle='None',label='U(x)')
+  # The plot in z dirction is done separately, because in HgBa2CuO4 we have different lattice spacing
+  # and I want to show that the fit works also in z direction. (So it is actually unnecessary for SVO)
+  #plt.plot(r_ext[nx,ny,:,2],U_ext[nx,ny,:,plotcomp],marker='*',linestyle='None',label='U(z)')
+  plt.legend()
+  plt.xlim(-10,10)
+  plt.xlabel('x,z')
+  f3.show()
 
 U_ext=U_ext.reshape((-1,ncomp))
 r_ext=r_ext.reshape((-1,3))
@@ -231,6 +250,11 @@ for i in xrange(nqx):
       qvec[i,j,k]=np.array([float(i)/(a*nqx),float(j)/(b*nqy),float(k)/(c*nqz)]) # the q grid has to be rescaled with lattice spacing
       Vq[i,j,k,:]=np.dot(np.exp(2*1j*np.pi*np.dot(r_ext,np.array([qvec[i,j,k]]).transpose())).transpose(),U_ext)
 
+Vq_sum=np.sum(Vq,axis=(0,1,2))/(nqx*nqy*nqz)
+print 'vq summation',Vq_sum
+Vq=Vq-Vq_sum
+
+
 print 'writing output to',outfilename_vq
 outfile_vq=h5py.File(outfilename_vq,'w')
 outfile_vq.create_group('.axes')
@@ -239,15 +263,16 @@ for igr,gr in enumerate(groups):
   outfile_vq[gr]=Vq[...,igr].reshape(-1)
 outfile.close()
 
-f4=plt.figure(4)
-plt.plot(Vq[:,0,0,plotcomp].real,linestyle='None',marker='.')
-plt.plot(Vq[:,0,0,plotcomp].imag,linestyle='None',marker='.')
-f4.show()
+if options.show_plots:
+  f4=plt.figure(4)
+  plt.plot(Vq[:,0,0,plotcomp].real,linestyle='None',marker='.')
+  plt.plot(Vq[:,0,0,plotcomp].imag,linestyle='None',marker='.')
+  f4.show()
 
 print Vq[:,0,0,0]
 
 print 'done, press Enter to exit'
 
 
-
-raw_input()
+if options.show_plots:
+  raw_input()
